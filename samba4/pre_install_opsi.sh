c465cc7984c1ce7068724505772c6abb5984c715
tobkern <tobkern1980@googlemail.com>
2014-06-25 09:37:32 +0200

    Create pre_install_opsi.sh

---
+++ samba4/pre_install_opsi.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+# Script-Name : .sh
+# Version     : 0.0.1
+# Autor       : Tobias Kern
+# Datum       : 25-06-2014
+# Lizenz      : GPLv3
+# Depends     : curl, 
+# Use         : execute it
+#
+# Description:
+# Source:
+#
+####################################################################################################
+## Notise: no tested
+##
+####################################################################################################
+#
+
+# 
+if [ `id -u` != 0 ]; then
+    echo "Please login as ROOT user to execute your script : $@!"
+    sleep 2
+    exit 1
+fi
+
+cat > << EOF /etc/apt/sources.list.d/opsi-configed.list
+#
+deb http://download.opensuse.org/repositories/home:/uibmz:/opsi:/opsi40/Debian_7.0/ /
+EOF
+#####
+
+# Add apt key for opsi
+KEYURL="http://download.opensuse.org/repositories/home:/uibmz:/opsi:/opsi40/Debian_7.0/Release.key"
+
+curl $KEYURL | apt-key add -
+
+exit 0
