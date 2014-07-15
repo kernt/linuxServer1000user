8794425748aab3221ac7a9492e68b4ce2ea7dee2
tobkern <tobkern1980@googlemail.com>
2014-07-15 14:55:55 +0200

    Create samba4_mods.sh

---
+++ opsi/samba4_mods.sh
@@ -0,0 +1,24 @@
+# if you have an error use following.
+#
+# modfi an user to start the opsiconfd deamon
+usermod -aG opsiconfd:shadow
+
+
+# users/groups and ID
+# User: opsiconfd = ID993
+# group: opsifileadmins
+# 
+
+# users/groups Rights
+# /etc/opsi/
+# -
+# -
+# -
+
+# /var/lib/opsi/ Rights
+# -
+# -
+# -
+# -
+
+
