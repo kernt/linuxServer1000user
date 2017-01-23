61144a4b080728e732efdba7deb8f92096eb468e
tobkern <tobkern1980@googlemail.com>
2017-01-23 09:55:30 +0100

    Create debian-install.sh

---
+++ samba4/debian-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get install build-essential libacl1-dev libattr1-dev \
+   libblkid-dev libgnutls-dev libreadline-dev python-dev \
+   python-dnspython gdb pkg-config libpopt-dev libldap2-dev \
+   dnsutils libbsd-dev attr krb5-user docbook-xsl libcups2-dev acl
+
+
+   apt-get install sernet-samba sernet-samba-ad sernet-samba-client
